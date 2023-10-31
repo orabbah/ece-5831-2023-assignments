cd6fbf6e164e7fee3dfae2709d17e0a27eadf2cf
orabbah <orabbah@gmail.com>
2023-10-30 22:06:55 -0400

    update1

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ def load_images(file_name):
     img = np.array(img)  # Convert to NumPy array
     img = img.reshape(1, -1)  # Flatten to (1, 784) shape
     return img
+<<<<<<< HEAD
 
 # List of image file names and their corresponding actual labels
 image_data = [
@@ -26,6 +27,20 @@ image_data = [
 ]
 
 correct_predictions = 0
+=======
+
+# Load and make predictions for each image
+image_files = ['2_0.jpg', '2_1.jpg', '2_2.jpg', '2_3.jpg', '2_4.jpg']
+
+for file_name in image_files:
+    x_test = load_images(file_name)
+    predics = np.argmax(net.predict(x_test), axis=1)
+    print("Prediction for", file_name, ":", predics)
+
+
+
+
+>>>>>>> 649a568 (update1)
 
 for file_name, actual_label in image_data:
     x_test = load_images(file_name)
